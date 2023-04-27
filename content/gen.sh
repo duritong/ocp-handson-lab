6ba42c6e38a9e3eaba1bac1db7bebb22eae31ba6
mh <mh@immerda.ch>
2023-04-27 08:44:32 +0200

    update url

--- content/gen.sh
+++ content/gen.sh
@@ -3,4 +3,5 @@
 cd $(readlink -f $(dirname $0))
 source ./vars
 
-cat index.md | sed -e "s/APPS_DOMAIN/${APPS_DOMAIN}/g" -e "s/IDP_NAME/${IDP_NAME}/g" | discount-mkd2html -css simple.min.css | sed 's@title><@title>OpenShift HandsOn<@' > index.html
+#cat index.md | sed -e "s/APPS_DOMAIN/${APPS_DOMAIN}/g" -e "s/IDP_NAME/${IDP_NAME}/g" | discount-mkd2html -css simple.min.css | sed 's@title><@title>OpenShift HandsOn<@' > index.html
+cat index.md | sed -e "s/APPS_DOMAIN/${APPS_DOMAIN}/g" | discount-mkd2html -css simple.min.css | sed 's@title><@title>OpenShift HandsOn<@' > index.html
